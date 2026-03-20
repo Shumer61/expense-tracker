4e88a7e902df4855ca1a6f2d26864d5d8fc2c093
shuma <39579339+Shumer61@users.noreply.github.com>
2026-03-20 13:57:31 +0300

    Add Express server with four CRUD routes for expenses

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const expenseRoutes = require('./routes/expenses');
+app.use('/expenses', expenseRoutes);
+
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Expense tracker API is running' });
+});
 app.listen(5000, ()=>{
     console.log('Server running on port 5000')
 });
